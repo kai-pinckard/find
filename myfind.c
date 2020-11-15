7ae28e117da39f4470710ddb760471ca1bf8f520
kai <kai.pinckard@gmail.com>
2020-11-14 18:34:06 -0800

    add mtime functionality

--- myfind.c
+++ myfind.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <fnmatch.h>
-
+#include <time.h>
 /*
 The file type and mode
        The  stat.st_mode field (for statx(2), the statx.stx_mode field) contains the file type and
@@ -32,6 +32,7 @@ The file type and mode
 
 // Globals consider removing
 char* pattern;
+int num_days;
 bool debug = true;
 
 // start using struct to store information.
@@ -43,9 +44,10 @@ typedef struct
     // for example file.txt
     // subsubdir
     const char* file_name;
-    bool isdir;
+    // stores the information from calling stat
+    struct stat statbuffer;
 
-} path_item;
+} file_data_t;
 
 /*
 Returns a new char* containing the directory with the last / removed
@@ -83,7 +85,7 @@ void print_match(const char* path)
 {
     int length = strlen(path);
 
-    if(path[length] == '/')
+    if(path[length - 1] == '/')
     {
         length--;
     }
@@ -95,7 +97,10 @@ void print_match(const char* path)
             perror("Can't write to stdout");
         }  
     }
-
+    if(putchar('\n') == EOF)
+    {
+        perror("Can't write to stdout");
+    }  
 }
 
 /*
@@ -108,14 +113,42 @@ bool handle_name(const char* pattern, const char* file_name)
     return fnmatch(pattern, file_name, 0) == 0;
 }
 
+
+
+/*
+    Returns true if the modified time is within num_days of the
+    current time.
+*/
+bool occurred_within(const time_t current_time, const time_t m_time, const int num_days)
+{
+    const long int num_secs_per_day = 86400;
+    long int max_dif = num_secs_per_day * num_days;
+    return (current_time - m_time) < max_dif;
+}
 /*
     Returns true if the file specified by file_name was modified at
     mtime. note +/- features not implemented. 
 */
-bool handle_mtime()
+bool handle_mtime(const file_data_t file)
 {
     //printf("handling mtime\n");
-    return true;
+    //struct tm* time = localtime(&(file.statbuffer.st_mtime));
+    //if (time != NULL)
+    //{
+    printf("it is %s \n", ctime(&(file.statbuffer.st_mtime)));
+    //printf("%s\n", file.path);
+    //printf("%ld\n\n", file.statbuffer.st_mtime);
+    
+    time_t cur_time;
+    time(&cur_time);
+    //current_time = localtime(&rawtime);
+    return occurred_within(cur_time, file.statbuffer.st_mtime, num_days);
+    //}
+    //else
+    //{
+    //perror("unable to convert time.");
+    //}
+    //free(time);
 }
 
 /*
@@ -156,22 +189,21 @@ bool handle_L()
     returns true if a file meets the requirements.
     returns false otherwise.
 */
-void handle_file(const char* file_name, bool is_dir)
+void handle_file(const file_data_t file)
 {
-    if(is_dir)
+    //if(is_dir)
+    //{
+    if(handle_name(pattern, file.file_name))
     {
-        //misleading file_name in here is actually a path
-        char* dir_file = dir_path_to_dir_name(file_name);
-        char* directory_no_slash = remove_last_slash(file_name);
-
-        if(handle_name(pattern, dir_file))
+        print_match(file.path);
+        if(handle_mtime(file))
         {
-            printf("matched %s\n", directory_no_slash);
+            printf("%s recent enough\n", file.path);
         }
-        free(directory_no_slash);
-        free(dir_file);
     }
-    else
+    //handle_mtime(file);
+    //}
+/*     else
     {
         //printf("handling file %s\n", file_name);
         if(!handle_name(pattern, file_name))
@@ -185,12 +217,12 @@ void handle_file(const char* file_name, bool is_dir)
         if(!handle_mtime(pattern, file_name))
         {
             return false;
-        }*/
+        }
         else
         {
             printf("%s\n", file_name);
         }
-    }
+    } */
 }
 
 /*
@@ -210,15 +242,14 @@ char* join_path(const char* directory, const char* file_name)
 /*
     Recursively traverses the directory applying the specified function on each file
 */
-void walk_dir(char* directory)
+void walk_dir(file_data_t dir_file_data)
 {
     struct dirent *de;
-    struct stat statbuffer;
-
+    
     // Every directory handles it self at the beginning
-    handle_file(directory, true);
+    handle_file(dir_file_data);
 
-    DIR* dr = opendir(directory);
+    DIR* dr = opendir(dir_file_data.path);
 
     if (dr == NULL)
     {
@@ -231,11 +262,13 @@ void walk_dir(char* directory)
         if ( (strcmp(de->d_name, "..") != 0 ) && (strcmp(de->d_name, ".") != 0) )
         {
             char* file_name = strdup(de->d_name);
-            char* path = join_path(directory, de->d_name);
+            char* path = join_path(dir_file_data.path, de->d_name);
+            struct stat statbuffer;
+            file_data_t cur_file = {path, file_name, statbuffer};
 
             // check if de->d_type is available for performance
             // fall back on calling stat.
-            if(stat(path, &statbuffer) == -1)
+            if(stat(path, &cur_file.statbuffer) == -1)
             {
                 perror("Unable to get stat info for input file");
             }
@@ -245,12 +278,12 @@ void walk_dir(char* directory)
             {
                 //printf("%s\n", path);
                 strcat(path, "/");
-                walk_dir(path);
+                walk_dir(cur_file);
             }
             else
             {
                 // None directories are handled here
-                handle_file(file_name, false);
+                handle_file(cur_file);
                 //printf("walking %s\n", path);
             }
             free(file_name);
@@ -314,7 +347,9 @@ char* parse_args(int argc, char** argv)
             }
             else if(strcmp(argv[i], "-mtime") == 0)
             {
-                handle_mtime();
+                //handle_mtime();
+                num_days = atoi(argv[i+1]);
+                printf("num days: %d\n", num_days);
             }
             else if(strcmp(argv[i], "-type") == 0)
             {
@@ -346,6 +381,17 @@ int main(int argc, char** argv)
 {
 
     parse_args(argc, argv);
-    walk_dir(argv[1]);
+    struct stat statbuffer;
+    file_data_t base_dir = 
+        {
+            argv[1],
+            dir_path_to_dir_name(argv[1]),
+            statbuffer
+        };
+    if(stat(base_dir.path, &base_dir.statbuffer) == -1)
+    {
+        perror("Unable to get stat info for input file");
+    }
+    walk_dir(base_dir);
     return 0;
 }
